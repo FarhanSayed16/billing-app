f503cc7f8b1906629c228def77bbb9a45132d02c
FarhanSayed16 <farhansayed54@gmail.com>
2026-05-15 23:53:10 +0530

    feat: add LoyaltyController with methods for managing loyalty balances and customer details

---
+++ backend/dist/src/loyalty/loyalty.controller.d.ts
@@ -0,0 +1,43 @@
+import { LoyaltyService } from './loyalty.service';
+export declare class LoyaltyController {
+    private readonly loyaltyService;
+    constructor(loyaltyService: LoyaltyService);
+    getPublicLoyaltyBalance(phone: string): Promise<{
+        balance: number;
+    }>;
+    getCustomerLoyalty(req: any, customerId: string): Promise<{
+        balance: number;
+        lifetimeEarned: number;
+        lifetimeRedeemed: number;
+        ledger: {
+            id: string;
+            created_at: Date;
+            type: import("@prisma/client").$Enums.LedgerType;
+            description: string | null;
+            customer_id: string;
+            invoice_id: string | null;
+            points: number;
+        }[];
+    }>;
+    getLoyaltyByPhone(req: any, phone: string): Promise<{
+        balance: number;
+        rupeeEquivalent: number;
+        eligibleToRedeem: boolean;
+        minRedemptionThreshold: number;
+        customer: {
+            name: string;
+            phone: string;
+        };
+        lifetimeEarned: number;
+        lifetimeRedeemed: number;
+        ledger: {
+            id: string;
+            created_at: Date;
+            type: import("@prisma/client").$Enums.LedgerType;
+            description: string | null;
+            customer_id: string;
+            invoice_id: string | null;
+            points: number;
+        }[];
+    }>;
+}
