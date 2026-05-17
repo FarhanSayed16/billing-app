7ceedeb758a9f7efa574ed42e23495a7652fc76f
FarhanSayed16 <farhansayed54@gmail.com>
2026-05-17 21:41:57 +0530

    feat: add LoyaltyService for managing customer loyalty and points

---
+++ backend/dist/src/loyalty/loyalty.service.d.ts
@@ -0,0 +1,45 @@
+import { PrismaService } from '../prisma/prisma.service';
+export declare class LoyaltyService {
+    private readonly prisma;
+    private readonly logger;
+    constructor(prisma: PrismaService);
+    getCustomerLoyalty(brandId: string, customerId: string): Promise<{
+        balance: number;
+        lifetimeEarned: number;
+        lifetimeRedeemed: number;
+        ledger: {
+            id: string;
+            created_at: Date;
+            type: import("@prisma/client").$Enums.LedgerType;
+            description: string | null;
+            customer_id: string;
+            invoice_id: string | null;
+            points: number;
+        }[];
+    }>;
+    getCustomerLoyaltyByPhone(brandId: string, phone: string): Promise<{
+        balance: number;
+        rupeeEquivalent: number;
+        eligibleToRedeem: boolean;
+        minRedemptionThreshold: number;
+        customer: {
+            name: string;
+            phone: string;
+        };
+        lifetimeEarned: number;
+        lifetimeRedeemed: number;
+        ledger: {
+            id: string;
+            created_at: Date;
+            type: import("@prisma/client").$Enums.LedgerType;
+            description: string | null;
+            customer_id: string;
+            invoice_id: string | null;
+            points: number;
+        }[];
+    }>;
+    getPublicLoyaltyByPhone(phone: string): Promise<{
+        balance: number;
+    }>;
+    expireOldPoints(): Promise<void>;
+}
