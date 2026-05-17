f8622a63df4dd5e2788a948e3ece6befded4731c
FarhanSayed16 <farhansayed54@gmail.com>
2026-05-17 21:43:13 +0530

    feat: add ReturnsController for managing return requests and approvals

---
+++ backend/dist/src/returns/returns.controller.d.ts
@@ -0,0 +1,71 @@
+import { ReturnsService } from './returns.service';
+import { CreateReturnDto } from './dto/create-return.dto';
+export declare class ReturnsController {
+    private readonly returnsService;
+    constructor(returnsService: ReturnsService);
+    createReturn(req: any, createReturnDto: CreateReturnDto): Promise<{
+        message: string;
+        returnRequest: {
+            id: string;
+            brand_id: string;
+            created_at: Date;
+            updated_at: Date;
+            store_id: string;
+            employee_id: string;
+            status: string;
+            invoice_id: string;
+            reason: string | null;
+            refund_amount: import("@prisma/client-runtime-utils").Decimal;
+            loyalty_points_reversed: number;
+        };
+    }>;
+    getPendingReturns(req: any): Promise<({
+        invoice: {
+            customer: {
+                name: string;
+                phone: string;
+            } | null;
+            billing_id: string;
+        };
+        items: ({
+            invoice_item: {
+                id: string;
+                name: string;
+                tax_amount: import("@prisma/client-runtime-utils").Decimal;
+                total: import("@prisma/client-runtime-utils").Decimal;
+                product_id: string | null;
+                quantity: number;
+                unit_price: import("@prisma/client-runtime-utils").Decimal;
+                tax_rate: import("@prisma/client-runtime-utils").Decimal;
+                returned_quantity: number;
+                invoice_id: string;
+            };
+        } & {
+            id: string;
+            quantity: number;
+            invoice_item_id: string;
+            return_request_id: string;
+        })[];
+        employee: {
+            name: string;
+        };
+    } & {
+        id: string;
+        brand_id: string;
+        created_at: Date;
+        updated_at: Date;
+        store_id: string;
+        employee_id: string;
+        status: string;
+        invoice_id: string;
+        reason: string | null;
+        refund_amount: import("@prisma/client-runtime-utils").Decimal;
+        loyalty_points_reversed: number;
+    })[]>;
+    approveReturn(req: any, id: string): Promise<{
+        message: string;
+    }>;
+    rejectReturn(req: any, id: string): Promise<{
+        message: string;
+    }>;
+}
