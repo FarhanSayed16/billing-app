408f5f733f80aa9bd142067fcdfd220848d9482c
FarhanSayed16 <farhansayed54@gmail.com>
2026-05-20 22:44:34 +0530

    feat: create AppModule with initial imports and configuration

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { StoresModule } from './stores/stores.module';
+import { EmployeesModule } from './employees/employees.module';
+import { CustomersModule } from './customers/customers.module';
+import { InvoicesModule } from './invoices/invoices.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    StoresModule,
+    EmployeesModule,
+    CustomersModule,
+    InvoicesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
